bf00d420132b0ba8bacb1bfc3decae1e217a4830
dayannaold <180118889@aluno.unb.br>
2022-12-04 20:44:19 -0800

    react

--- system/myapp/src/App.js
+++ system/myapp/src/App.js
@@ -1,9 +1,100 @@
-export default function App(){
+/*
+import React from "react";
+
+const Team = (props) => {
+  return (
+    <div>
+      <About username={props.name} 
+      role={props.role}
+      age={props.age}/>
+      <Social fb={props.facebook}/>
+      <hr/>
+    </div> 
+
+  );
+}
+
+const About = (props) => {
   return(
     <div>
-      <h1>Welcome to React App
-        <h2>@dayannaold</h2>
-      </h1>
+      <h2>Hello, I'm {props.username}</h2>
+      <h3>I'm a {props.role}</h3>
+      <h3>I'm {props.age} Years.</h3> 
     </div>
-  )
+  );
 }
+// const Welcome = (props) => {
+//   return(
+//     <div>
+//       <h2>Welcome {props.name}</h2>
+//       <h3>I have {props.age} Years.</h3>
+//       </div>
+//   );
+// }
+
+const Social = (props) => {
+  return(
+    <div>
+      <a href={props.fb}> Facebook</a>
+      <a>| LinkedIn </a>
+      <a>| Youtube </a>
+    </div>
+
+  );
+}
+function App(){
+  return(
+    <div>
+      //{<Welcome name="Dayanna" age="27"/>}
+      <h1>Know our team:</h1>
+      <Team name="Dayanna" role="Coding" age="27"
+      facebook="https://facebook.com"/>
+      <Team name="Janet" role="Designer" age="30"
+      facebook="https://facebook.com"/>
+        <Team name="Amanda" role="Front-end" age="24"
+      facebook="https://facebook.com"/>
+    </div>
+  );
+}
+export default App;
+*/
+import React, { Component } from 'react';
+
+class Team extends Component {
+    render(){
+      return(
+      <div>
+        <h2> Hello. I'm {this.props.name}. </h2>
+        <h2> Role: {this.props.role} .</h2>
+        <h2> I'm {this.props.age} Years.</h2>
+      </div>
+      );
+    }
+}
+class About extends Component{
+  render(){
+    return(
+      <div>
+      <h2> Hello. I'm {this.props.name}. </h2>
+      <h2> Role: {this.props.role} .</h2>
+      <h2> I'm {this.props.age} Years.</h2>
+    </div>
+    )
+
+  }
+
+}
+function App(){
+  return(
+    <div>
+      <h1>Know our team:</h1>
+      <hr/>
+      <Team name="Dayanna" role="Coding" age="27"/>
+      <hr/>
+      <Team name="Janet" role="Marketing" age="30"/>
+    </div>
+
+  );
+}
+
+export default App;
